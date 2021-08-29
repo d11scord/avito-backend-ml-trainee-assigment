80c135247dfea9cd2d3cece5955b7f915a7f5a5e
Julia Kuzkina <juliakuzkina1@gmail.com>
2021-08-29 23:11:45 +0300

    Added fetch_matrix and clean_matrix funcs

--- app/matrix.py
+++ app/matrix.py
@@ -9,3 +9,21 @@ async def fetch_matrix(url: str):
             print("Response status:", response.status)
             raw_matrix = await response.text()
             print(raw_matrix)
+            print(clean_matrix(raw_matrix))
+
+
+def clean_matrix(raw_matrix: str):
+    raw_matrix = raw_matrix.split("\n")
+    raw_matrix = [
+        row.replace("+", "").replace("-", "").replace("|", "").split()
+        for row in raw_matrix[1::2]  # Скипаем +---+---+---+---+
+    ]
+    matrix = list()
+
+    for row in raw_matrix:
+        # TODO: откуда-то берётся пустой массив
+        if not row:
+            continue
+        matrix.append(list(map(int, row)))
+
+    return matrix
